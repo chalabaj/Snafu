473330ee7d6b137c0c7e71d1a1333fe9881de3af
chalabaj <chalabaj@vscht.cz>
2018-07-09 09:56:10 +0200

    desc

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 """
-SNAFU code
-SNAFU can do ab initio nonadiabatic MD withot calculating non-adiabatic couplings
-Two scheme with/without diabatization
-
-1) adiabatic-potential-based formula derived within Landau-Zener:
-AK Belyaev, PHYSICAL REVIEW A 84, 014701 (2011),DOI: 10.1103/PhysRevA.84.014701
-AK Belyaev, The Journal of Chemical Physics 147, 234301 (2017); doi: 10.1063/1.5000718
-
-2) diabatization scheme: Le Yu, Phys.Chem.Chem.Phys., 2014, 16, 25883
+main rutine
 """
 
 import sys
