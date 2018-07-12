ae1d7a573048c30caa29b9b066d91b98d1795e2c
chalabaj <chalabaj@vscht.cz>
2018-07-12 09:58:16 +0200

    Add files via upload

--- snafu/print.py
+++ snafu/print.py
@@ -7,9 +7,10 @@ def print_positions(step,time,natoms, at_names, x, y, z):
      mov.write(natoms,"\n")
      mov.write("Step: ",step,"     ","Time_fs: ",time,"\n")
      for iat in range(0,natoms):
+      
       line = (' {0: }{0:2f.3} {0:2f.3} {0:2f.3}\n'.format(x[iat], y[iat], z[iat]))
-
+      line = "  ".join("%1s %1d" "%1d" "%1d" %(x[iat], y[iat], z[iat]))
      mov.write(line)
-     mov.closed
+    mov.closed
     return()
 
