52462043cf00f9c063da4ef2ba2f7a3b2da448fd
chalabaj <chalabaj@vscht.cz>
2018-09-18 13:55:32 +0200

    defaults

--- ABINITIO/orca-tddft.sh
+++ ABINITIO/orca-tddft.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-cd ABINITIO
+#cd ABINITIO
 
-source ../SetEnvironment.sh ORCA 4.0.0
+source SetEnvironment.sh ORCA 4.0.0
 ##########SNAFU INPUTS###########################################
 abinit_geom_file=$1
 natoms=$2
 state=$3   # for which state
-nstate=$4 # total number of state
+let nstate=$4-1 # total number of state minus ground state which is calculated separately
 step=$5
 input=input.com
+
 #SYSTEM:
+mem=8000   # memory in MB per core
 charge=-3 
 multiplicity=6
-basis=LANL2DZ  # def2-SVP
-DFTfunc="B3LYP"
+basis=LANL2DZ         # def2-SVP and 6-31+g** are about the same expensive
+DFT="B3LYP RIJCOSX"   # TightSCF 
 #RI-JK approximations for hybrid-DFT methods-: 
 #RIJCOSX approximation for Coulomb and HF Exchange is recommended, but RIJK can be better for small molecules
 #E(DFT-TDDFT) with RIJCOSX for wat dimer is 0.0001
 #E(DFT-TDDFT) with NORI for wat dimer is 0.0000
 
 
-mem=8000   # memory in MB per core
+if [[ $state -eq 0 ]];then
+ gstask="! ENGRAD $DFT"
+ extask="! ENERGY $DFT"
+else
+ gstask="! ENERGY $DFT"
+ extask="! ENGRAD $DFT"
+ iroot="iroot $state"
+fi
 
-if [[ ! -z  {$NSLOTS}]]; then
+if [[ ! -z {$NSLOTS} ]];then
 nprocs=$NSLOTS
 else
 nprocs=1
@@ -30,7 +39,6 @@ fi
 ###########END OF INPUTS
 
 cat > $input << EOF
-
 %maxcore $mem          # memory per core
 %pal 
    nprocs $nprocs           # number of cores
@@ -40,44 +48,29 @@ end
 Basis "$basis"        # The orbital expansion basis set
 end
 
-! ENGRAD $DFTfunc  RIJCOSX # for small systems increase accuracy by: Grid5 FinalGrid6 
-! AUTOSTART TightSCF   # try to read from previous step
+! $gstask           # for small systems increase accuracy by: Grid5 FinalGrid6 
+! AUTOSTART         # try to read from previous step
 * xyz $charge $multiplicity 
 EOF
 tail -n$natoms ../$abinit_geom_file >> $input
 echo '*' >>$input
 
-if [[ $state -eq 0 ]];then
-
 cat >> $input << EOF
 $new_job
-! $DFTfunc RIJCOSX TightSCF
+! $extask
 ! moread
 %basis
- Basis "LANL2DZ"        # The orbital expansion basis set
+ Basis "$basis"        # The orbital expansion basis set
 end
 %tddft
   maxdim 5
-  nroots 5
-  maxcore 32000
+  nroots $nstate
+  maxcore $mem
+  $iroot  
 end
 * xyz $charge $multiplicity 
 EOF
 tail -n$natoms ../$abinit_geom_file >> $input
-else 
-%tddft
-  maxdim 5
-  nroots $nstate
-  iroot $state
-  maxcore 32000
-end
-fi
-
-export TMPDIR=$PWD/scratch
-
-if [[ ! -d $TMPDIR ]]; then
-mkdir $TMPDIR
-fi
 
 $ORCAEXE $input &> $input.out
 ################################
@@ -94,5 +87,5 @@ awk -v natom="$natom" '{if ($5=="energy") {getline;getline;print $1}
 if ($4=="gradient") {getline;
 	for(i=1;i<=natom;i++) {
 		getline; x=$1;getline;y=$1;getline;print x,y,$1
- }}}' $input.engrad > ../engrad.dat.$ibead
+ }}}' $input.engrad > ../engrad.dat 
 
