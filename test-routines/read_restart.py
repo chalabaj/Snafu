8df864799ca1680297b2c9e6380ae12417232ed5
chalabaj <chalabaj@vscht.cz>
2018-10-23 13:15:48 +0200

    read rest file

--- test-routines/read_restart.py
+++ test-routines/read_restart.py
@@ -2,7 +2,13 @@ import numpy as np
 
 def read_restart():
     #with open restart.in as rsf:
-    np.loadtxt("restart.in", dtype=np.float64, delimiter=None, skiprows=8)  
+    rst_file = "restart.in"
+    with open(rst_file, 'r') as rstf:
+        step = rstf.readline().split()[1]
+        print(step)
+    rstf.closed
+    a = np.loadtxt(rst_file, dtype=np.float64, delimiter=None, skiprows=8)  
+    print(a)
     return()
     
 if __name__ == "__main__":
